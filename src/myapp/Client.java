5b7c4d097dd9e73d9f72f767c2c343473f997228
monishajg <monishajg@gmail.com>
2022-12-12 21:19:07 +0800

    first commit

---
+++ src/myapp/Client.java
@@ -0,0 +1,53 @@
+package myapp;
+
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        int PORT = 12345;
+        try {
+            Socket cs = new Socket("localhost", PORT);
+
+            // Get the I/O steams
+            OutputStream os = cs.getOutputStream();
+            BufferedOutputStream bos = new BufferedOutputStream(os);
+            DataOutputStream dos = new DataOutputStream(bos);
+
+            // dos.writeUTF("hello");
+            // dos.writeUTF("world");
+            // dos.flush();
+            // System.out.println("MESSAGE SENT TO SERVER");
+            // cs.close();
+
+            Scanner inputSc = new Scanner(System.in);
+            String line;
+            while (( line = inputSc.nextLine()) != null) {
+                if (line.equalsIgnoreCase("close")) {
+                    System.out.println("Exit from shell");
+                    dos.writeUTF("close");
+                    dos.flush();
+                    break;
+                }
+
+                dos.writeUTF(line);
+                dos.flush();
+                System.out.println("MSG sent to clien : " + line);
+
+            }
+            cs.close(); // closing the socket from client.
+            inputSc.close();
+
+        } catch (UnknownHostException e) {
+            System.out.println("Unable to reach the HOST");
+        } catch (IOException e) {
+            System.out.println("IO Error");
+        }
+        
+    }
+}
